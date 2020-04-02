17f98fda907929911af3147c7a103f4c9a4bf32a
Marco Domingos <supersthotaku@gmail.com>
2020-04-02 15:03:56 +0100

    Refine movement system, changes needed on script PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
     // Defines the playerRigid body to add forces and control them
     Rigidbody2D playerBody;
     // Defines the normal Force Multiplier
-    float forceMultiplier = 10.0f;
+    float forceMultiplier = 20.0f;
+    float airForceMultiplier = 3.0f;
     // A flag that defines the jump buffer
     int jumpBufferFlag = 0;
+    //
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +25,40 @@ public class PlayerMovement : MonoBehaviour
     {
         // Defines the Horizontal axis according to player input.
         float hAxis = Input.GetAxis("Horizontal");
-        //float vAxis = Input.GetAxis("Vertical");
-        Vector2 movementVect = new Vector2(hAxis * forceMultiplier, 0);
+        float vAxis = Input.GetAxis("Vertical");
+
+        // Obtains the Movement Vector and calculates the x axis velocity with it
+        float movementVect = hAxis * forceMultiplier;
+        Vector2 currentVelocity = playerBody.velocity;
+        currentVelocity.x = movementVect;
 
         // Adds a force with the horizontal axis player input.
-        playerBody.AddForce(movementVect);
+        // Needs to: 
+        //*NIMP*    Increase gravity pull so it goes up faster and falls EVEN faster
+        //
+        //*NIMP*    Make it so that changing directions on the air does not snap to the 
+        //          other side (ADDED, needs to put a velocity cap to make it consistent)
+        // 
+        //*NIMP*    Make it so when player stops on the ground it does not stops IMMEDI-
+        //          ATELY. Give it few milisseconds of a "slide"
+        //
+        //*IMP*     Add functional double jump (w/ flags with the isGrounded)
+        if (isGrounded)
+            playerBody.velocity = currentVelocity;
+        else
+        {
+            if (hAxis != 0)
+            {
+                currentVelocity.x *= airForceMultiplier;
+                playerBody.AddForce(currentVelocity);
+            }
+        }
+
+        Debug.Log(isGrounded);
 
         // Observes if the Up Arrow is pressed and adds +1 to the flag in
         // each frame.
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (vAxis > 0)
         {
             jumpBufferFlag += 1;
         }
@@ -41,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
         // not yet landed.
         if (jumpBufferFlag >= 25)
         {
-            playerBody.AddForce(new Vector2(0, 300));
+            playerBody.velocity = new Vector2(0, 10);
 
             Debug.Log("Get BIG!");
             Debug.Log(jumpBufferFlag);
@@ -52,9 +80,9 @@ public class PlayerMovement : MonoBehaviour
 
         // If the jump flag is equal or higher than 1 and the Up Arrow is NOT
         // pressed, the player will perform a short jump.
-        if (jumpBufferFlag >= 1 && !Input.GetKey(KeyCode.UpArrow))
+        if (jumpBufferFlag >= 1 && (vAxis == 0))
         {
-            playerBody.AddForce(new Vector2(0, 200));
+            playerBody.velocity = new Vector2(0, 5);
 
             Debug.Log("Get SMALL!");
             Debug.Log(jumpBufferFlag);
@@ -64,6 +92,24 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //Debug.Log("Force Multiplier:" + forceMultiplier);
-        //Debug.Log("H/V Axis:" + movementVect);
+        Debug.Log("H/V Axis:" + hAxis + " " + vAxis);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Entered");
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Exited");
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
