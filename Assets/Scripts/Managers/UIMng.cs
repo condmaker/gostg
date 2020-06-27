1c6dd0969ee547165b77ca6ef0dfb3dbf76ac588
Miguel-Martinho <miguelpm2001@gmail.com>
2020-06-27 22:46:27 +0100

    updated tileset level 2, more menu changes

--- Assets/Scripts/Managers/UIMng.cs
+++ Assets/Scripts/Managers/UIMng.cs
@@ -5,33 +5,70 @@ using UnityEngine;
 public class UIMng : MonoBehaviour
 {
     public GameObject[] pauseUI;
-    public GameObject Menu;
+    public GameObject menu;
+    public GameObject selector;
+
+    public int[] selector_positions;
+    private int distanceToMove;
+    private int posIndex;
+    private float arrowInput;
 
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        distanceToMove = 100;
+        selector_positions[0] = 146;
+        posIndex = 0;
+        for (int i = 1; i < 4; i++)
+            {
+            selector_positions[i] = selector_positions[i - 1] - distanceToMove;
+            }
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("in update");
         if (Input.GetKeyDown("escape"))
         {
             if (Time.timeScale == 1)
             {
                 Time.timeScale = 0;
                 Pause();
-                Menu.SetActive(true);
+                menu.SetActive(true);
             }
             else if (Time.timeScale == 0)
             {
                 Time.timeScale = 1;
                 Unpause();
-                Menu.SetActive(false);
+                menu.SetActive(false);
             }
         }
-        if (Input.GetKeyDown("arrow down") && Time.timeScale == 0)
+        if (Input.GetButton("Down") && Time.timeScale == 0)
+        {
+            Debug.Log("clicked");
+            if (posIndex >= 4)
+            {
+                posIndex = 0;
+            }
+            else
+            {
+                selector.transform.position = new Vector3(selector.transform.position.x, selector_positions[posIndex] - distanceToMove ,selector.transform.position.z);
+            }
+        }
+        if (Input.GetButton("Up") && Time.timeScale == 0)
+        {
+            Debug.Log(selector_positions[0]);
+            if (posIndex >= 4)
+            {
+                posIndex = 0;
+            }
+            else
+            {
+                selector.transform.position = new Vector3(selector.transform.position.x, selector_positions[posIndex] - distanceToMove, selector.transform.position.z);
+            }
+        }
 
 
     }
