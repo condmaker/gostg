422f6d2c354db9eb29bd7900216ce8160fc3cb78
Marco Domingos <supersthotaku@gmail.com>
2020-06-28 03:18:57 +0100

    Start Araya1

---
+++ Assets/Scripts/Level&scenery/Araya1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class Araya1 : MonoBehaviour
+{
+    private CinemachineVirtualCamera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Testy1");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Testy2");
+            cam.LookAt = transform;
+        }
+    }
+}
