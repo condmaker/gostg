396258cd861b4046cc317ac2ac2701659fa2ed9d
Miguel-Martinho <miguelpm2001@gmail.com>
2020-06-28 10:02:00 +0100

    Finished cutscenes with touko on placeholder

---
+++ Assets/Scripts/PlayCredits.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayCredits : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExecuteAfterTime(69));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        text.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayNextscene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayNextscene : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExecuteAfterTime(3.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        SceneManager.LoadScene("Cutscene 2");
+    }
+}
